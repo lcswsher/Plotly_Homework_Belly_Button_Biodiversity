fc48f94d5c0ea5589b4c5856e2c8965fbcd8d03d
lcswsher <lcswisher@yahoo.com>
2021-08-01 12:43:38 -0500

    updates to bonus and app files - one file to be used app, bonus file was dropped

--- app.js
+++ app.js
@@ -10,6 +10,10 @@
 //      URL scheme "samples.json - file" is not supported and must be an http").
 // 
 // Demographic Info Chart - MetaData (id, ethnicity, gender, age, location, bbtype, wfreq) is displayed for a selected subject id
+
+// Color palette for Gauge Chart
+var arrColorsG = ["#5899DA", "#E8743B", "#19A979", "#ED4A7B", "#945ECF", "#13A4B4", "#525DF4", "#BF399E", "#6C8893", "white"];
+
 function metadataSample(sample) {
     d3.json("samples.json").then((data) => {
         var metadata = data.metadata;
@@ -22,6 +26,99 @@ function metadataSample(sample) {
             panel.append("h6").text(`${key}: ${value}`);
         });
     });
+}
+
+function buildGaugeChart(sample) {
+    console.log("sample", sample);
+
+    d3.json("samples.json").then(data => {
+
+        var objs = data.metadata;
+        //console.log("objs", objs);
+
+        var matchedSampleObj = objs.filter(sampleData =>
+            sampleData["id"] === parseInt(sample));
+        //console.log("buildGaugeChart matchedSampleObj", matchedSampleObj);
+
+        gaugeChart(matchedSampleObj[0]);
+    });
+}
+
+function gaugeChart(data) {
+    console.log("gaugeChart", data);
+
+    if (data.wfreq === null) {
+        data.wfreq = 0;
+
+    }
+
+    let degree = parseInt(data.wfreq) * (180 / 10);
+
+    // Trig to calc meter point
+    let degrees = 180 - degree;
+    let radius = .5;
+    let radians = degrees * Math.PI / 180;
+    let x = radius * Math.cos(radians);
+    let y = radius * Math.sin(radians);
+
+    let mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+    let path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+    let trace = [{
+        type: 'scatter',
+        x: [0], y: [0],
+        marker: { size: 50, color: '2F6497' },
+        showlegend: false,
+        name: 'WASH FREQ',
+        text: data.wfreq,
+        hoverinfo: 'text+name'
+    },
+    {
+        values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+        rotation: 90,
+        text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+        textinfo: 'text',
+        textposition: 'inside',
+        textfont: {
+            size: 16,
+        },
+        marker: { colors: [...arrColorsG] },
+        labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '2-1', '0-1', ''],
+        hoverinfo: 'text',
+        hole: .5,
+        type: 'pie',
+        showlegend: false
+    }];
+
+    let layout = {
+        shapes: [{
+            type: 'path',
+            path: path,
+            fillcolor: '#2F6497',
+            line: {
+                color: '#2F6497'
+            }
+        }],
+
+        title: '<b>Belly Button Washing Frequency</b> <br> <b>Scrub Per Week</b>',
+        height: 550,
+        width: 550,
+        xaxis: {
+            zeroline: false, showticklabels: false,
+            showgrid: false, range: [-1, 1]
+        },
+        yaxis: {
+            zeroline: false, showticklabels: false,
+            showgrid: false, range: [-1, 1]
+        },
+    };
+
+    Plotly.newPlot('gauge', trace, layout, { responsive: true });
+
 }
 
 // Horizontal Bar and Bubble Charts  
@@ -73,7 +170,7 @@ function Charts(sample) {
 
         var barLayout = {
             title: "Top 10 Bacteria Cultures",
-            margin: {t: 40, l:150}
+            margin: { t: 40, l: 150 }
         };
 
         Plotly.newPlot("bar", bar_data, barLayout);
@@ -99,6 +196,8 @@ function init() {
         const firstSample = sample_data_Names[0];
         Charts(firstSample);
         metadataSample(firstSample);
+        buildGaugeChart(firstSample)
+
     });
 
 }
@@ -106,6 +205,7 @@ function init() {
 function optionChanged(newSample) {
     Charts(newSample);
     metadataSample(newSample);
+    buildGaugeChart(newSample)
 }
 
 init();
